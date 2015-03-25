e1673a13034670bf8db96b4f780e384ac4c0b462
ycartc <tracymcaruso@gmail.com>
2015-03-24 19:09:04 -0600

    Colorized Guesses

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,6 +1,32 @@
 require 'sinatra'
+require 'sinatra/base'
 require 'sinatra/reloader'
 
-get '/' do
-  erb :index, :locals => {:number => number}
-end
+
+  NUMBER = rand(101)
+  BUFFER = 5
+
+  get '/' do
+    guess = params["guess"].to_i
+    message = check_guess(guess)
+    erb :index, :locals => {:number => NUMBER, :message => message, :bg_color => @bg_color}
+  end
+
+  def check_guess(guess)
+    case
+    when guess > (NUMBER + BUFFER)
+      @bg_color = "red"
+      "Way too high!"
+    when guess > (NUMBER)
+      @bg_color = "pink"
+      "Too high!"
+    when guess < (NUMBER - BUFFER)
+      @bg_color = "red"
+      "Way too low!"
+    when guess < (NUMBER)
+      @bg_color = "pink"
+    else
+      @bg_color = "green"
+      "Remarkable, You got it right!"
+    end
+  end
